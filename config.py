f003f1553fd3e3e0a40d1eb49110da4ae5f698e4
atulpatel99357-ai <atulpatel99357@gmail.com>
2026-05-31 11:10:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,21 +6,21 @@ load_dotenv()
 
 class Config:
     def __init__(self):
-        self.API_ID = int(getenv("API_ID", "17596251"))
-        self.API_HASH = getenv("API_HASH", "e58343b4c0193e293e391daf97603fcd")
+        self.API_ID = int(getenv("API_ID", "26676259"))
+        self.API_HASH = getenv("API_HASH", "a6194f7e8ec4adf87e5a50c7e9335f5c")
 
-        self.BOT_TOKEN = getenv("BOT_TOKEN", "Apna Bot Token")
+        self.BOT_TOKEN = getenv("BOT_TOKEN", "8869365977:AAF7ozd4DB38nu9Y_q_v7FccRoemE8AV0eo")
         self.MONGO_URL = getenv("MONGO_URL", "Apna Mongo Db Dalo")
 
-        self.LOGGER_ID = int(getenv("LOGGER_ID", "Apna Log Group Id Dalo"))
-        self.OWNER_ID = int(getenv("OWNER_ID", "Owner I'd dalo"))
+        self.LOGGER_ID = int(getenv("LOGGER_ID", "-1003864442012"))
+        self.OWNER_ID = int(getenv("OWNER_ID", "8619158508"))
         
-        self.SESSION1 = getenv("SESSION", "Apna String Dalo")
+        self.SESSION1 = getenv("SESSION", "BQGXDCMAHLc2VQJz2mG6vAyTchsTtgeOCe_yWtEBOCl-vombLb57UV0Zcy6COvjywe9awKMKcZ-Miopf-YC0Ofl41YfovztCiPZNWe185szFZV5BghuMzkIb1GzgSu4pCcsRDFQ9EVZ8Ma37y0oIQodghmIZ2ZMP_NGQwoL6CH75swlj1wl8k2kPyMgRVSzoSb9ZQM0NW0LZN4tJEbNjDtVEu5AVW3a6jsutOdI_LcGmLRmJmZdRNJAIm7tB3rYAhqWorE4hg3SajH4Nwh9VrVx1VpGrWFmNCageJpHBCPX08OVVAcs4IcQLtu1anE3aBchxftZdHW9IvOVevCZu6OCglA1DLwAAAAIBvevsAA")
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AloneUpdates")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AloneBotSupport")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PatelGxCheat")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+jY4N29HHCT5kMTE1")
 
         self.AUTO_END: bool = getenv("AUTO_END", False)
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
